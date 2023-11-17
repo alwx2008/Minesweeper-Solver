e7a5c70ab69887c97152b8bc0260ecba1b9192fd
werewolf lord <40964777+alwx2008@users.noreply.github.com>
2023-11-17 22:47:41 +0000

    Add startGame() method

--- Game/MineSweeper.java
+++ Game/MineSweeper.java
@@ -10,3 +10,19 @@ public class MineSweeper {
         M.startGame();
     }
 }
+
+public void startGame() {
+    System.out.println("\n\n================Welcome to Minesweeper ! ================\n");
+    setupField(1);
+
+    boolean flag = true;
+    while (flag) {
+        displayVisible();
+        flag = playMove();
+        if(checkWin()) {
+            displayHidden();
+            system.out.println("\n================You WON!!!====================");
+            break;
+        }
+    }
+}
